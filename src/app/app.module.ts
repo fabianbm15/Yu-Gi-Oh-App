69b0623f129b39d7bd3d061edcd3364524e07f8f
Carlos Fabián Melgarejo Agudelo <cmelgarejobm@gmail.com>
2023-11-04 22:14:33 -0500

    Home structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { ListComponent } from './pages/list/list.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
