7d7b94c31bdf6c4dfb7c7cac72a1c3a1729e8789
Carlos Fabián Melgarejo Agudelo <cmelgarejobm@gmail.com>
2023-11-08 12:05:01 -0500

    Detail Page

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Card } from 'src/app/interfaces/card.interface';
 
 @Component({
@@ -8,4 +9,10 @@ import { Card } from 'src/app/interfaces/card.interface';
 })
 export class CardComponent {
   @Input() card!: Card;
+
+  constructor(private router: Router) {}
+
+  goToCard() {
+    this.router.navigate([`./card/${this.card.id}`]);
+  }
 }
